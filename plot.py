171722d0d9c324e8c5bd3a557862020c3434676a
axect <axect.tg@proton.me>
2024-05-31 16:03:36 +0900

    Plot grad

--- plot.py
+++ plot.py
@@ -13,6 +13,7 @@ eta_vec = [
     eta_init + delta_eta * (np.sqrt((N - x) / U * (2 - (N + x) / U)) - np.sqrt(N / U * (2 - N / U)))
     for x, N in zip(epoch_vec, N_vec)
 ]
+eta_grad_vec = delta_eta / np.sqrt(N_vec / U * (2 - N_vec / U)) * (- 2 / U)
 
 # Plot params
 pparam = dict(
@@ -30,4 +31,21 @@ with plt.style.context(["science", "nature"]):
     for i in range(len(N_vec)):
         ax.plot(epoch_vec[i], eta_vec[i], label=f'N = {N_vec[i]}')
     ax.legend()
-    fig.savefig('plot.png', dpi=600, bbox_inches='tight')
+    fig.savefig('plot_eta.png', dpi=600, bbox_inches='tight')
+
+# Plot params
+pparam = dict(
+    xlabel = r'N',
+    ylabel = r'$\eta^\prime(0)$',
+    xscale = 'linear',
+    yscale = 'linear',
+)
+
+# Plot
+with plt.style.context(["science", "nature"]):
+    fig, ax = plt.subplots()
+    ax.autoscale(tight=True)
+    ax.set(**pparam)
+    ax.plot(N_vec, eta_grad_vec)
+    ax.legend()
+    fig.savefig('plot_grad.png', dpi=600, bbox_inches='tight')
