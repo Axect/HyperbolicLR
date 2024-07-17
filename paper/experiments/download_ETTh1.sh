2ade03cbcb3372dad58ce985758163910c9a98bf
axect <axect.tg@proton.me>
2024-07-17 13:24:08 +0900

    Remove unnecessary codes

--- paper/experiments/download_ETTh1.sh
+++
@@ -1,2 +0,0 @@
-wget https://github.com/zhouhaoyi/ETDataset/raw/main/ETT-small/ETTh1.csv
-mv ETTh1.csv data/./
--- paper/experiments/mamba.sh
+++
@@ -1,5 +0,0 @@
-git clone https://github.com/Axect/mamba.py mamba_py
-
-mv mamba_py/mamba.py ./
-mv mamba_py/pscan.py ./
-rm -rf mamba_py
